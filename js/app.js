109007c771e38104e68296bd9f755c14796be6b5
Oleksa Koba <oleksakoba@gmail.com>
2018-12-04 01:12:29 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,26 @@
+var displayWikiData = function() {
+
+	var $linksElement = $('#links');
+	var searchString = $('#searchString').val();
+	var wikipediaUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + searchString + '&format=json&callback=wikiCallback';
+
+	$.ajax({
+		url: wikipediaUrl,
+		dataType: 'jsonp',
+		jsonp: 'callback',
+		success: function(res){
+			var linkLists = res[1];
+			linkLists.forEach(function(item) {
+				var url = 'https://en.wikipedia.org/wiki/' + item;
+				$linksElement.append('<li><a href="' + url +'">' + item + '</a></li>');
+				return url;
+			})
+		}
+	})
+
+	return false;
+}
+$('#form').submit(displayWikiData);
+
+
+
